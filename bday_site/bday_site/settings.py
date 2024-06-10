5d6acdb916d62c3838bc38fd9f8cad92107cdef8
temporary-push-ya <dontbeanidiot1234@gmail.com>
2024-06-10 11:22:29 -0700

    debug_off w/ fix allowed hosts

--- bday_site/bday_site/settings.py
+++ bday_site/bday_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-iog__hv(5o_!kvd7r@z+uttj^2$(ylmvqem$z@8h1!_i#258#3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost','https://bday-2024-customwordcloudsite.onrender.com','*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost','bday-2024-customwordcloudsite.onrender.com','*']
 
 
 # Application definition
