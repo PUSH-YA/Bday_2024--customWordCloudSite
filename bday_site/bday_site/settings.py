19063c0a5a6e6c2867444a5b099bfa321ef355d4
temporary-push-ya <dontbeanidiot1234@gmail.com>
2024-06-10 00:11:52 -0700

    trying debugged mode

--- bday_site/bday_site/settings.py
+++ bday_site/bday_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-iog__hv(5o_!kvd7r@z+uttj^2$(ylmvqem$z@8h1!_i#258#3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost','https://bday-2024-customwordcloudsite.onrender.com','*']
 
