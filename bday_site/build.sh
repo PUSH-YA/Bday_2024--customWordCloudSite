bf41a03fc51361db7b75aa46de3cf1530093e270
temporary-push-ya <dontbeanidiot1234@gmail.com>
2024-06-14 20:44:22 -0700

    updated it to work with ec2 instance

--- bday_site/build.sh
+++ bday_site/build.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
+
+# update env
+sudo apt-get update
+
+# install pip3
+sudo apt-get install -y python3-pip
+
+# create a virtual environment
+sudo apt-get install -y python3-venv
+python3 -m venv bday
+source bday/bin/activate
+# deactivate to deactivate the virtual environment
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
+# Convert static asset files -> not needed if DEBUGGING = TRUE
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# runserver
+python3 manage.py runserver 0.0.0.0:8000
 
 # running locally:
 # python -m gunicorn mysite.asgi:application -k uvicorn.workers.UvicornWorker
